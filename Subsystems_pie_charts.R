560e2d8bd13b8e6492c284a59b0648adcc36fc3d
eKariuki-sleepy <55273010+eKariuki-sleepy@users.noreply.github.com>
2022-01-24 12:12:56 +0300

    R.scripts -orig

---
+++ Subsystems_pie_charts.R
@@ -0,0 +1,123 @@
+# Subsystems_pie_charts.R
+# Created 3/03/2017, by Sam Westreich
+# Last updated 6/16/2017
+# Run with --help flag for help.
+
+suppressPackageStartupMessages({
+  library(optparse)
+})
+
+option_list = list(
+  make_option(c("-I", "--input"), type="character", default="./",
+              help="Input directory", metavar="character"),
+  make_option(c("-O", "--out"), type="character", default="Subsys_pie_chart.pdf", 
+              help="output image name; [default= %default]", metavar="character"),
+  make_option(c("-L", "--level"), type="integer", default=1,
+              help="level of Subsystems hierarchy for pie chart; [default=%default]", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+print("USAGE: $ Subsystems_pie_charts.R -I working_directory/ -O save.filename -L level (1,2,3,4)")
+
+# check for necessary specs
+if (is.null(opt$input)) {
+  print ("WARNING: No working input directory specified with '-I' flag.")
+  stop()
+} else {  cat ("Working directory is ", opt$input, "\n")
+  wd_location <- opt$input  
+  setwd(wd_location)  }
+
+cat ("Saving results as ", opt$out, "\n")
+save_filename <- opt$out
+
+cat ("Creating pie chart for hierarchy level ", opt$level, "\n")
+
+# import other necessary packages
+suppressPackageStartupMessages({
+  library("ggplot2")
+  library("data.table")
+})
+
+# get list of files
+files_list <- list.files( pattern = "*.reduced", full.names = T, recursive = FALSE)
+file_names = ""
+for (name in files_list) {
+  file_names <- c(file_names, strsplit(name, split='./')[1])}
+file_names <- file_names[-1]
+file_names_trimmed = ""
+for (name in file_names) {
+  file_names_trimmed <- c(file_names_trimmed, unlist(strsplit(name, split='.', fixed=TRUE))[1])}
+file_names_trimmed <- file_names_trimmed[-1]
+
+# loading the files in
+y <- 0
+for (x in files_list) {
+  y <- y + 1
+  if (y == 1) {
+    data_table <- read.table(file = x, header = F, quote = "", sep = "\t", fill = TRUE)
+    colnames(data_table) = c("DELETE", x, "Level4", "Level3", "Level2", "Level1")
+    data_table <- data_table[,-1]
+    rownames(data_table) <- data_table$Level4 }
+  if (y > 1) {
+    temp_table <- read.table(file = x, header = F, quote = "", sep = "\t", fill = TRUE)
+    colnames(temp_table) = c("DELETE", x, "Level4", "Level3", "Level2", "Level1")
+    rownames(temp_table) <- temp_table$Level4
+    temp_table <- temp_table[,c(2,3)]
+    data_table <- merge(temp_table, data_table, by = "Level4")  
+    }
+}
+if (is.na(data_table[1,ncol(data_table)])) {
+  data_table <- data_table[,-ncol(data_table)]
+}
+
+# At this point, the whole table is read in.  Next step (for graphs) is to consolidate it
+# down to averages.
+data_table$Average <- rowSums(data_table[,c(1:length(files_list)+1)]) / length(files_list)
+data_table <- data_table[order(-data_table$Average),]
+# remove blank spots (no hierarchy)
+data_table <- data_table[-which(data_table$Level1 == ""), ]
+
+# Now, the next block run will change based on which level is wanted for the final pie chart:
+if (opt$level == 1) {
+  l1_table <- data.table(data_table[,c("Average","Level1")])
+} else if (opt$level == 2) {
+  l1_table <- data.table(data_table[,c("Average","Level2")])
+  colnames(l1_table) <- c("Average", "Level1")
+} else if (opt$level == 3) {
+  l1_table <- data.table(data_table[,c("Average","Level3")])
+  colnames(l1_table) <- c("Average", "Level1")
+} else if (opt$level == 4) {
+  l1_table <- data.table(data_table[,c("Average","Level4")])
+  colnames(l1_table) <- c("Average", "Level1")
+}
+
+# need to add an extra check if there are blank entries
+if (sum(l1_table$Level1 == "") > 0) {
+  l1_table <- l1_table[-which(l1_table$Level1 == ""), ]
+}
+l1_table <- l1_table[, lapply(.SD, sum), by=Level1]
+l1_table <- l1_table[order(-l1_table$Average)]
+
+# Pie chart time?
+CbPalette <- c("#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", 
+      "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a", "#ffff99", "#b15928", "#8dd3c7",  
+      "#ffffb3",  "#bebada",  "#fb8072",  "#80b1d3",  "#fdb462",  "#b3de69", 
+      "#fccde5",  "#d9d9d9",  "#bc80bd",  "#ccebc5",  "#ffed6f", "#e41a1c",  
+      "#377eb8", "#4daf4a",  "#984ea3",  "#ff7f00", "#ffff33",  "#a65628",  
+      "#f781bf", "#999999", "#000000", "#a6cee4", "#1f78b5", "#b2df8b")
+
+bp<- ggplot(l1_table[c(0:10),], aes(x="", y=Average, fill=Level1))+
+  geom_bar(width = 1, stat = "identity") +
+  scale_fill_manual(values = CbPalette) +
+  ggtitle("SEED Subsystems hierarchy")
+
+pie <- bp + coord_polar("y", start=0) +
+#    geom_text(aes(x=1, label=Level1)) +
+    theme(axis.text=element_blank(), legend.position = "right" ) 
+
+cat ("\nSuccess!\nSaving Subsystems pie chart as ", save_filename, " now.\n")
+pdf(file = save_filename, width=10, height=7)
+pie
+dev.off()
---
+++ make_DESeq_heatmap.R
@@ -0,0 +1,167 @@
+# make_DESeq_heatmap.R
+# Created 6/27/16
+# Last updated 6/16/2017
+# Run with --help flag for help.
+
+suppressPackageStartupMessages({
+  library(optparse)
+})
+
+option_list = list(
+  make_option(c("-I", "--input"), type="character", default="A:/Analy2is/R_analysis/counts/Organism",
+              help="Input directory", metavar="character"),
+  make_option(c("-N", "--entryCount"), type="integer", default=60,
+              help="Number of row entries [default= %default]", metavar="character"),
+  make_option(c("-O", "--out"), type="character", default="DESeq_heatmap.pdf", 
+              help="output file name [default= %default]", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+print("USAGE: $ make_DESeq_heatmap -I working_directory/ -O save.filename -N number of row entries (50)")
+
+# check for necessary specs
+if (is.null(opt$input)) {
+  print ("WARNING: No working input directory specified with '-I' flag.")
+  stop()
+} else {  cat ("Working directory is ", opt$input, "\n")
+  wd_location <- opt$input  
+  setwd(wd_location)  }
+
+cat ("Saving results as ", opt$out, "\n")
+save_filename <- opt$out
+
+cat ("Loading packages, may take a second...\n")
+# import other necessary packages
+suppressPackageStartupMessages({
+  library(DESeq2)
+  library("pheatmap")
+  library(genefilter)
+  library(RColorBrewer)
+  library(ggplot2)
+})
+
+# GET FILE NAMES
+control_files <- list.files(
+  pattern = "control_*", full.names = T, recursive = FALSE)
+control_names = ""
+for (name in control_files) {
+  control_names <- c(control_names, unlist(strsplit(name, split='_', fixed=TRUE))[2])}
+control_names <- control_names[-1]
+control_names_trimmed = ""
+for (name in control_names) {
+  control_names_trimmed <- c(control_names_trimmed, unlist(strsplit(name, split='.', fixed=TRUE))[1])}
+control_names_trimmed <- control_names_trimmed[-1]
+
+exp_files <- list.files(
+  pattern = "experimental_*", full.names = T, recursive = FALSE)
+exp_names = ""
+for (name in exp_files) {
+  exp_names <- c(exp_names, unlist(strsplit(name, split='_', fixed=TRUE))[2])}
+exp_names <- exp_names[-1]
+exp_names_trimmed = ""
+for (name in exp_names) {
+  exp_names_trimmed <- c(exp_names_trimmed, unlist(strsplit(name, split='.', fixed=TRUE))[1])}
+exp_names_trimmed <- exp_names_trimmed[-1]
+
+# READ IN FILES
+# loading the control table
+y <- 0
+for (x in control_files) {
+  y <- y + 1
+  if (y == 1) {
+    control_table <- read.table(file = x, header=F, quote = "", sep = "\t", fill = TRUE)
+    if (ncol(control_table) == 4) {
+      colnames(control_table) = c("DELETE", x, "V3", "md5")
+      control_table <- control_table[,c(4,2,3)] 
+    } else {
+      colnames(control_table) = c("DELETE", x, "V3")
+      control_table <- control_table[,c(3,2)] }
+    if (nrow(control_table) > 1000) {
+      control_table <- control_table[c(1:1000),] }}     # can be deleted, restricts to top 1k hits
+  if (y > 1) {
+    temp_table <- read.table(file = x, header = F, quote = "", sep = "\t", fill = TRUE)
+    if (nrow(temp_table) > 1000) {
+      temp_table <- temp_table[c(1:1000),] }      # can be deleted, restricts to top 1k hits
+    print (x)
+    if (ncol(temp_table) == 4) {
+      colnames(temp_table) = c("DELETE", x, "V3", "md5") 
+    } else {
+      colnames(temp_table) = c("DELETE", x, "V3") }
+    control_table <- merge(control_table, temp_table[,c(2,3)], by = "V3", all.x = T)  }
+}
+control_table[is.na(control_table)] <- 0
+rownames(control_table) = control_table$V3
+control_table_trimmed <- control_table[,-1, drop = FALSE]
+
+# loading the exp table
+y <- 0
+for (x in exp_files) {
+  y <- y + 1
+  if (y == 1) {
+    exp_table <- read.table(file = x, header=F, quote = "", sep = "\t", fill = TRUE)
+    if (ncol(exp_table) == 4) {
+      colnames(exp_table) = c("DELETE", x, "V3", "md5")
+      exp_table <- exp_table[,c(4,2,3)] 
+    } else {
+      colnames(exp_table) = c("DELETE", x, "V3")
+      exp_table <- exp_table[,c(3,2)] }
+    if (nrow(exp_table) > 1000) { exp_table <- exp_table[c(1:1000),] }} # can be deleted, restricts to top 1k hits
+  if (y > 1) {
+    temp_table <- read.table(file = x, header = F, quote = "", sep = "\t", fill = TRUE)
+    if (nrow(temp_table) > 1000) { temp_table <- temp_table[c(1:1000),] }    # can be deleted, restricts to top 1k hits
+    print (x)
+    if (ncol(temp_table) == 4) {
+      colnames(temp_table) = c("DELETE", x, "V3", "md5") 
+    } else {
+      colnames(temp_table) = c("DELETE", x, "V3") }
+    exp_table <- merge(exp_table, temp_table[,c(2,3)], by = "V3", all.x = T)  }
+}
+exp_table[is.na(exp_table)] <- 0
+rownames(exp_table) = exp_table$V3
+exp_table_trimmed <- exp_table[,-1]
+
+# getting the column names simplified
+colnames(control_table_trimmed) = control_names_trimmed
+colnames(exp_table_trimmed) = exp_names_trimmed
+
+complete_table <- merge(control_table_trimmed, exp_table_trimmed, by=0, all = TRUE)
+complete_table[is.na(complete_table)] <- 1
+rownames(complete_table) <- complete_table$Row.names
+complete_table <- complete_table[,-1]
+completeCondition <- data.frame(condition=factor(c(
+  rep(paste("control", 1:length(control_files), sep=".")), 
+  rep(paste("experimental", 1:length(exp_files), sep=".")))))
+completeCondition1 <- t(completeCondition)
+colnames(complete_table) <- completeCondition1
+completeCondition2 <- data.frame(condition=factor(c(
+  rep("control", length(control_files)), 
+  rep("experimental", length(exp_files)))))
+
+#Add sample ID as Row_names _EK
+completeCondition2 = data.frame(row.names = c("CF1","CF3","CF4","BSG1","BSG2","BSG3","CM1","CM2","CM3","FM1","FM2","FM3","WH1","WH2","WH3"),completeCondition2)
+
+cat ("Calculating DESeq results, may take a second...\n")
+dds <- DESeqDataSetFromMatrix(complete_table, completeCondition2, ~condition)
+
+dds <- DESeq(dds)
+
+transformed_data <- rlog(dds, blind=FALSE)
+topVarGenes <- head(order(rowVars(assay(transformed_data)), decreasing=T), opt$entryCount)
+
+# making the PCA plot
+
+# calculate euclidean distances from the variance-stabilized data
+matrix <- assay(transformed_data)[ topVarGenes, ]
+matrix <- matrix - rowMeans(matrix)
+
+# Create heatmap of distances
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+heatmap <- pheatmap(matrix)
+
+# saving and finishing up
+cat ("Saving heatmap as ", save_filename, " now.\n")
+pdf(file = save_filename, width=7, height=10)
+heatmap
+dev.off()
