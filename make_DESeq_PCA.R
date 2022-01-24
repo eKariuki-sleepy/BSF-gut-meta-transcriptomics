32e19022b3d0a585f5667d0f5332685c68b1a914
eKariuki-sleepy <55273010+eKariuki-sleepy@users.noreply.github.com>
2022-01-24 12:08:03 +0300

    R.scripts -orig

---
+++ make_DESeq_PCA.R
@@ -0,0 +1,158 @@
+# make_DESeq_PCA.R
+# Created 6/28/16
+# Last updated 6/16/2017
+# Run with --help flag for help.
+
+suppressPackageStartupMessages({
+  library(optparse)
+})
+
+option_list = list(
+  make_option(c("-I", "--input"), type="character", default="A:/Analy2is/R_analysis/counts/Organism",
+              help="Input directory", metavar="character"),
+  make_option(c("-O", "--out"), type="character", default="PCA_plot.tab",
+              help="output file name [default= %default]", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+print("USAGE: $ run_DESeq_stats.R -I working_directory/ -O save.filename -L level (1,2,3,4)")
+
+# check for necessary specs
+if (is.null(opt$input)) {
+  print ("WARNING: No working input directory specified with '-I' flag.")
+  stop()
+} else {  cat ("Working directory is ", opt$input, "\n")
+  wd_location <- opt$input
+  setwd(wd_location)  }
+
+cat ("Saving results as ", opt$out, "\n")
+save_filename <- opt$out
+
+cat ("Calculating DESeq results for hierarchy level ", opt$level, "\n")
+
+# import other necessary packages
+suppressPackageStartupMessages({
+  library(DESeq2)
+  library("pheatmap")
+  library(ggplot2)
+  library(factoextra)
+  library(ggforce)
+  library(ggrepel)
+})
+
+# GET FILE NAMES
+control_files <- list.files(
+  pattern = "control_*", full.names = T, recursive = FALSE)
+control_names = ""
+for (name in control_files) {
+  control_names <- c(control_names, unlist(strsplit(name, split='_', fixed=TRUE))[2])}
+control_names <- control_names[-1]
+control_names_trimmed = ""
+for (name in control_names) {
+  control_names_trimmed <- c(control_names_trimmed, unlist(strsplit(name, split='.', fixed=TRUE))[1])}
+control_names_trimmed <- control_names_trimmed[-1]
+
+exp_files <- list.files(
+  pattern = "experimental_*", full.names = T, recursive = FALSE)
+exp_names = ""
+for (name in exp_files) {
+  exp_names <- c(exp_names, unlist(strsplit(name, split='_', fixed=TRUE))[2])}
+exp_names <- exp_names[-1]
+exp_names_trimmed = ""
+for (name in exp_names) {
+  exp_names_trimmed <- c(exp_names_trimmed, unlist(strsplit(name, split='.', fixed=TRUE))[1])}
+exp_names_trimmed <- exp_names_trimmed[-1]
+
+# READ IN FILES
+# loading the control table
+y <- 0
+for (x in control_files) {
+  y <- y + 1
+  if (y == 1) {
+    control_table <- read.table(file = x, header = F, quote = "", sep = "\t")
+    colnames(control_table) = c("DELETE", x, "V3")
+    control_table <- control_table[,c(2,3)]      }
+  if (y > 1) {
+    temp_table <- read.table(file = x, header = F, quote = "", sep = "\t")
+    colnames(temp_table) = c("DELETE", x, "V3")
+    temp_table <- temp_table[,c(2,3)]
+    control_table <- merge(control_table, temp_table, by = "V3", all = T)  }
+}
+control_table[is.na(control_table)] <- 0
+rownames(control_table) = control_table$V3
+control_table_trimmed <- control_table[,-1]
+
+# loading the experimental table
+y <- 0
+for (x in exp_files) {
+  y <- y + 1
+  if (y == 1) {
+    exp_table <- read.table(file = x, header=F, quote = "", sep = "\t")
+    colnames(exp_table) = c("DELETE", x, "V3")
+    exp_table <- exp_table[,c(2,3)]  }
+  if (y > 1) {
+    temp_table <- read.table(file = x, header = F, quote = "", sep = "\t")
+    colnames(temp_table) = c("DELETE", x, "V3")
+    exp_table <- merge(exp_table, temp_table[,c(2,3)], by = "V3", all = T)  }
+}
+exp_table[is.na(exp_table)] <- 0
+rownames(exp_table) = exp_table$V3
+exp_table_trimmed <- exp_table[,-1]
+
+# getting the column names simplified
+colnames(control_table_trimmed) = control_names_trimmed
+colnames(exp_table_trimmed) = exp_names_trimmed
+
+complete_table <- merge(control_table_trimmed, exp_table_trimmed, by=0, all = TRUE)
+complete_table[is.na(complete_table)] <- 1
+rownames(complete_table) <- complete_table$Row.names
+complete_table <- complete_table[,-1]
+completeCondition <- data.frame(condition=factor(c(
+  rep(paste("control", 1:length(control_files), sep=".")),
+  rep(paste("experimental", 1:length(exp_files), sep=".")))))
+completeCondition1 <- t(completeCondition)
+colnames(complete_table) <- completeCondition1
+completeCondition2 <- data.frame(condition=factor(c(
+  rep("control", length(control_files)),
+  rep("experimental", length(exp_files)))))
+
+#Add an extra field for SampleID_ EK
+
+completeCondition2 = data.frame(row.names = c("CF1","CF3","CF4","BSG1","BSG2","BSG3", "CM1", "CM2", "CM3","FM1","FM2","FM3","WH1","WH2","WH3"),completeCondition2)
+###convert rowname to column
+completeCondition2 <- tibble::rownames_to_column(completeCondition2, "samples")
+### Add new column with condition names
+completeCondition2$conditions <- c("CF","CF","CF","BSG","BSG","BSG","CM","CM","CM","FM","FM","FM","WH","WH","WH")
+dds <- DESeqDataSetFromMatrix(complete_table, completeCondition2, ~conditions)
+
+dds <- DESeq(dds)
+transformed_data <- rlog(dds, blind=FALSE)
+
+# making the PCA plot
+
+# calculate euclidean distances from the variance-stabilized data
+dists <- dist(t(assay(transformed_data)))
+PCAplot <- plotPCA(transformed_data, intgroup = "conditions", returnData = TRUE)
+percentVar <- round(100 * attr(PCAplot, "percentVar"))
+
+# saving and finishing up
+#cat ("Saving PCA plot as ", save_filename, " now.\n")
+#pdf(file = paste(save_filename,".pdf",sep = ""), width=10, height=7)
+pcplot <- ggplot(PCAplot, aes(PC1, PC2, color=conditions)) +
+    geom_point(size=3) +geom_label_repel(aes(label=completeCondition2$samples)) +
+#    geom_text(aes(label=name), hjust=1, vjust=-1) +
+    ggtitle("PCA Plot of control vs. experimental organism data") +
+    theme(legend.position = "bottom") +
+    xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+    ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  ##### Adding ellipses using ggforce ######
+  ggforce::geom_mark_ellipse(aes(fill = conditions,
+                               color = conditions)) +
+  theme(legend.position = 'bottom') +
+  coord_equal()
+  
+
+
+dev.off()
---
+++ run_DESeq_stats_func.R
@@ -0,0 +1,182 @@
+# Created 6/22/16, updated 6/16/2017
+# Run with --help flag for help.
+
+suppressPackageStartupMessages({
+  library(optparse)
+})
+
+option_list = list(
+  make_option(c("-I", "--input"), type="character", default="A:/Analy2is/R_analysis/counts/Function",
+              help="Input directory", metavar="character"),
+  make_option(c("-O", "--out"), type="character", default="A:/Analy2is/R_analysis/counts/DESeq_results_func.tab", 
+              help="output file name [default= %default]", metavar="character"),
+  make_option(c("-R", "--raw_counts"), type="character", default="A:/Analy2is/R_analysis/counts/rawcounts.txt",
+              help="raw (total) read counts for this starting file", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+print("USAGE: $ run_DESeq_stats.R -I working_directory/ -O save.filename")
+
+# check for necessary specs
+if (is.null(opt$input)) {
+  print ("WARNING: No working input directory specified with '-I' flag.")
+  stop()
+} else {  cat ("Working directory is ", opt$input, "\n")
+  wd_location <- opt$input  
+  setwd(wd_location)  }
+
+if (is.null(opt$out)) {
+  print ("WARNING: No save name for DESeq results specified; defaulting to 'DESeq_results.tab'.") 
+  save_filename <- opt$out
+} else { save_filename <- opt$out }
+
+if (is.null(opt$raw_counts)) {
+  print ("WARNING: no raw counts file specified, skipping this info for DESeq analysis.")
+} else {
+  counts_file <- opt$raw_counts
+}
+
+# import other necessary packages
+suppressPackageStartupMessages({
+  library(DESeq2)
+})
+
+# GET FILE NAMES
+control_files <- list.files(
+  pattern = "control_*", full.names = T, recursive = FALSE)
+control_names = ""
+for (name in control_files) {
+  control_names <- c(control_names, unlist(strsplit(name, split='_', fixed=TRUE))[2])}
+control_names <- control_names[-1]
+control_names_trimmed = ""
+for (name in control_names) {
+  control_names_trimmed <- c(control_names_trimmed, unlist(strsplit(name, split='.', fixed=TRUE))[1])}
+control_names_trimmed <- control_names_trimmed[-1]
+
+exp_files <- list.files(
+  pattern = "experimental_*", full.names = T, recursive = FALSE)
+exp_names = ""
+for (name in exp_files) {
+  exp_names <- c(exp_names, unlist(strsplit(name, split='_', fixed=TRUE))[2])}
+exp_names <- exp_names[-1]
+exp_names_trimmed = ""
+for (name in exp_names) {
+  exp_names_trimmed <- c(exp_names_trimmed, unlist(strsplit(name, split='.', fixed=TRUE))[1])}
+exp_names_trimmed <- exp_names_trimmed[-1]
+
+# sanity check
+if (length(exp_files) == 0 || length(control_files) == 0) {
+  print ("\nWARNING: No files found.  Is the directory correct?  Are the files named with 'control_' and 'experimental_' as prefixes?")
+  stop()
+}
+
+# READ IN FILES
+# loading the control table
+y <- 0
+for (x in control_files) {
+  y <- y + 1
+  if (y == 1) {
+    control_table <- read.table(file = x, header = F, quote = "", sep = "\t")
+    colnames(control_table) = c("DELETE", x, "V3")
+    control_table <- control_table[,c(3,2)]      }
+  if (y > 1) {
+    temp_table <- read.table(file = x, header = F, quote = "", sep = "\t")
+    colnames(temp_table) = c("DELETE", x, "V3")
+    temp_table <- temp_table[,c(2,3)]
+    control_table <- merge(control_table, temp_table, by = "V3", all = T)  }
+}
+control_table[is.na(control_table)] <- 0
+rownames(control_table) = control_table$V3
+control_table_trimmed <- data.frame(control_table[,-1])
+# this next step is for if there's only 1 control file (no replicates)
+if (y == 1) {
+  rownames(control_table_trimmed) = rownames(control_table)
+}
+
+# loading the experimental table
+z <- 0
+for (x in exp_files) {
+  z <- z + 1
+  if (z == 1) {
+    exp_table <- read.table(file = x, header=F, quote = "", sep = "\t")
+    colnames(exp_table) = c("DELETE", x, "V3")
+    exp_table <- exp_table[,c(2,3)]  }
+  if (z > 1) {
+    temp_table <- read.table(file = x, header = F, quote = "", sep = "\t")
+    colnames(temp_table) = c("DELETE", x, "V3")
+    exp_table <- merge(exp_table, temp_table[,c(2,3)], by = "V3", all = T)  }
+}
+exp_table[is.na(exp_table)] <- 0
+rownames(exp_table) = exp_table$V3
+exp_table_trimmed <- exp_table[,-1]
+
+# getting the column names simplified
+colnames(control_table_trimmed) = control_names_trimmed
+colnames(exp_table_trimmed) = exp_names_trimmed
+
+# merging the two tables
+complete_table <- merge(control_table_trimmed, exp_table_trimmed, by=0, all = TRUE)
+complete_table[is.na(complete_table)] <- 0
+# reducing stuff down to avoid duplicates
+complete_table <- aggregate(. ~  Row.names, data = complete_table, sum)
+rownames(complete_table) <- complete_table$Row.names
+complete_table <- complete_table[!(complete_table$Row.names == ""), ]
+# removing extra Row.names column
+complete_table <- complete_table[,-1]
+
+# OPTIONAL: importing the raw counts
+if (is.null(opt$raw_counts) == FALSE) {
+  raw_counts_table <- read.table(counts_file, header=FALSE, sep = "\t", quote = "")
+  raw_counts_table <- data.frame(raw_counts_table, 
+        do.call(rbind, strsplit(as.character(raw_counts_table$V1),'_')))
+  raw_counts_table$X2 <- as.numeric(as.character(raw_counts_table$X2))
+  raw_counts_table <- t(raw_counts_table[,c("X2", "V2")])
+  row.names(raw_counts_table) <- c("SAMPLE","RAW TOTAL")
+  colnames(raw_counts_table) <- raw_counts_table[1,]
+  raw_counts_table <- as.data.frame(raw_counts_table)
+  raw_counts_table <- raw_counts_table[-1,]
+  
+  # Need to subtract off the total number of annotations
+  raw_counts_table["ANNOTATION COUNT",] <- colSums(complete_table)
+  raw_counts_table["OTHER",] <- raw_counts_table[1,] - raw_counts_table[2,]
+  ####### my fix #########
+  data3 <- raw_counts_table["OTHER",]         # Replicate data
+  colnames(data3) <- colnames(complete_table) # Change column names
+  data3                                 # Print updated data
+  
+  complete_table <- rbind(complete_table, data3)
+}
+
+
+# DESeq statistical calculations
+completeCondition <- data.frame(condition=factor(c(
+  rep(paste("control", 1:length(control_files), sep=".")), 
+  rep(paste("experimental", 1:length(exp_files), sep=".")))))
+completeCondition1 <- t(completeCondition)
+colnames(complete_table) <- completeCondition1
+completeCondition2 <- data.frame(condition=factor(c(
+  rep("control", length(control_files)), 
+  rep("experimental", length(exp_files)))))
+
+dds <- DESeqDataSetFromMatrix(complete_table, completeCondition2, ~condition)
+dds <- DESeq(dds)
+
+# This step creates the summary results output
+res <- results(dds)
+org_results <- data.frame(res)
+# these next steps won't work if there's only 1 control sample (no replicates)
+if (y > 1) {
+  baseMeanPerLvl <- sapply( levels(dds$condition), function(lvl) rowMeans( counts(dds,normalized=TRUE)[,dds$condition == lvl] ) )
+  org_results <- merge(org_results, baseMeanPerLvl, by="row.names")
+  org_results <- org_results[,c(1,2,8,9,3,4,5,6,7)]
+  colnames(org_results)[c(3,4)] <- c("controlMean", "experimentalMean")
+}
+
+sorted_org_results <- org_results[order(-org_results$baseMean),]
+colnames(sorted_org_results)[1] <- "Organism Name"
+
+# saving and finishing up
+cat ("\nSuccess!\nSaving results file as ", save_filename, "\n")
+write.table(sorted_org_results, file = save_filename, append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
